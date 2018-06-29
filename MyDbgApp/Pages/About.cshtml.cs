7baf999c8bc7160187516a02c3aad854a3873170
Alexis Goupillot <hathor.development@gmail.com>
2018-06-29 11:33:11 +0200

    Modèle de projet basique chargé

--- MyDbgApp/Pages/About.cshtml.cs
+++ MyDbgApp/Pages/About.cshtml.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+
+/// <summary>
+///     Application apprentissage debogage Visual Studio | Link => https://docs.microsoft.com/fr-fr/visualstudio/debugger/quickstart-debug-aspnet
+/// </summary>
+
+
 namespace MyDbgApp.Pages
 {
     public class AboutModel : PageModel
